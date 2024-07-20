7116a5a283e93197d15285a46efac0f0a0097c6a
BirvaRaval <birvaraval.imscit20@gmail.com>
2024-07-20 14:59:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//  type writing effect------------------------------------------
+
+
+var typingEffect = new Typed(".multitext", {
+   strings: ["UI/UX Designer", "Developer", "Business Analyst", "Content Writer"],
+   loop: true,
+   typeSpeed: 80,
+   backSpeed: 5,
+   startDelay: 100,
+   backDelay: 100
+
+
+});
